d931529b750f3c726ef76f39c032072039fb1901
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2017-02-26 09:52:36 -0800

    Adding HipChat integration

--- src/bot.js
+++ src/bot.js
@@ -1,20 +1,37 @@
 'use strict';
 
 const _ = require('lodash');
+const http = require('superagent');
 
 module.exports.hello = (event, context, callback) => {
   const usersName = _.get(event, 'queryStringParameters.name', '');
   const message = _.trim(`Hi ${usersName}`);
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: message,
-      input: event,
-    }),
-  };
+  http
+    .post('https://h34t.hipchat.com/v2/room/3622915/notification')
+    .query({'auth_token': 'XXXXXX'})
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .send({
+      'color':'green',
+      'message':'My first notification (yey)',
+      'notify':false,
+      'message_format':'text'
+    })
+    .end(function(err, res) {
+      const response = {
+        statusCode: err ? 500 : 200,
+        body: JSON.stringify({
+          message: message,
+          err: err,
+          res: res,
+        }),
+      };
 
-  callback(null, response);
+      callback(null, response);
+    });
+
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
