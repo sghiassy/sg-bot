51f11804c56246f122a2b7faaff6399d70042551
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2017-02-26 22:31:30 -0800

    Adding logging

--- src/bot.js
+++ src/bot.js
@@ -6,20 +6,37 @@ const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const HIPCHAT_AUTH_TOKEN = process.env.HIPCHAT_AUTH_TOKEN;
 
+
+
 module.exports.hello = (event, context, callback) => {
-  const usersName = _.get(event, 'queryStringParameters.name', '');
-  const message = _.trim(`Hi ${usersName}`);
+  const usersName = _.get(event, 'queryStringParameters.name', undefined);
+  const points = _.get(event, 'queryStringParameters.points', undefined);
+  const message = _.trim(`User:${usersName} gets ${points}points`);
+  const requestHasInsufficientInfo = usersName === undefined || points === undefined;
+
+  if (requestHasInsufficientInfo) {
+    const message = 'Error 3458gdxh: Your request is missing required info';
+    console.warn(message);
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: message
+      })
+    });
+  }
 
   http
     .post('https://h34t.hipchat.com/v2/room/3622915/notification')
-    .query({'auth_token': HIPCHAT_AUTH_TOKEN})
+    .query({
+      'auth_token': HIPCHAT_AUTH_TOKEN
+    })
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
     .send({
-      'color':'green',
-      'message':message,
-      'notify':false,
-      'message_format':'text'
+      'color': 'green',
+      'message': message,
+      'notify': false,
+      'message_format': 'text'
     })
     .end(function(err, res) {
       const response = {
@@ -31,11 +48,21 @@ module.exports.hello = (event, context, callback) => {
         }),
       };
 
-      const dbEntry = {
-          TableName: 'Persons',
-          Item: {'1': 'Test'},
+      var params = {
+        TableName: 'Persons',
+        Item: {
+          'name': usersName,
+          'points': points,
+        }
       };
 
-      dynamo.put(dbEntry, callback(null, response));
+      dynamo.put(params, function(err, res) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(res);
+        }
+        callback(null, response);
+      });
     });
 };
